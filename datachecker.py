b3b2a6abe0c865be86ac662b28a64003644ba10d
Allen Tu <atu1@umd.edu>
2020-09-17 18:13:46 -0700

    Moved datachecker.py to base directory
    Moved datachecker.py to base directory for easier .gitignore. Updated its code to match.

--- datachecker.py
+++ datachecker.py
@@ -15,12 +15,13 @@ def png_checker(path, images_required):
 
 # Check if DIV2K is a complete dataset with all of its subsets and images. 
 dataset = 'DIV2K'
+dataset_path = 'data/' + dataset
 print('Checking ' + dataset + ' dataset...')
-if path.exists(dataset):
+if path.exists(dataset_path):
     # Check if DIV2K contains all required low-resolution subsets.
     subsets_LR = ['DIV2K_train_LR_bicubic', 'DIV2K_train_LR_unknown', 'DIV2K_valid_LR_bicubic', 'DIV2K_valid_LR_unknown']
     for subset in subsets_LR:
-        subset_path = dataset + '/' + subset
+        subset_path = dataset_path + '/' + subset
         if path.exists(subset_path):
             # Check if each subset contains all required scales. 
             scales = ['X2', 'X3', 'X4']
@@ -40,7 +41,7 @@ if path.exists(dataset):
     # Check if DIV2K contains all required high-resolution subsets.
     subsets_HR = ['DIV2K_train_HR', 'DIV2K_valid_HR']
     for subset in subsets_HR:
-        subset_path = dataset + '/' + subset
+        subset_path = dataset_path + '/' + subset
         if path.exists(subset_path):
             # Check if each subset contains the required number of images. 
             if subset_path.find('train') != -1:
@@ -49,6 +50,6 @@ if path.exists(dataset):
             else:
                 png_checker(subset_path, 100)
         else:
-            print(subset_path + 'is MISSING!')
+            print(subset_path + ' is MISSING!')
 else:
     print("DIV2K is a MISSING dataset")
