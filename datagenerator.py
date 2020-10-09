4c2685823c4c9908bd97510791f8e6e2389d94fc
Allen Tu <atu1@umd.edu>
2020-10-09 12:09:21 -0700

    Fixed some errors
    Changed 'import random' to 'from random import randint' because randint is only called on its own. 
    
    Check if 'data/trainingdata' already exists before calling mkdir so that an exception isn't thrown if it does. 
    
    Fixed string on Line 208.

--- datagenerator.py
+++ datagenerator.py
@@ -3,17 +3,17 @@ import os.path
 from os import path
 import glob  
 import cv2
-import random
+from random import randint
 
 num_size_training_set = 500
 training_set = [0 for x in range(num_size_training_set)]
 
 dataset_800_X2 = {'data/DIV2K/DIV2K_train_LR_bicubic/X2', 'data/DIV2K/DIV2K_train_LR_unknown/X2', 'data/Set14/LRbicx2', 'data/Set5/LRbicx2'}
- 
+
 dataset_800_X3 = {'data/DIV2K/DIV2K_train_LR_bicubic/X3',  'data/DIV2K/DIV2K_train_LR_unknown/X3', 'data/Set14/LRbicx3', 'data/Set5/LRbicx3'}
- 
+
 dataset_800_X4 = {'data/DIV2K/DIV2K_train_LR_bicubic/X4',   'data/DIV2K/DIV2K_train_LR_unknown/X4','data/Set14/LRbicx4', 'data/Set5/LRbicx4'} 
- 
+
 dataset_all = {'data/General100', 'data/BSDS100', 'data/urban100', 'data/Set14/GTmod12', 'data/Set14/original', 'data/Set5/GTmod12', 'data/Set5/original', 
                 'data/BSDS200',  'data/manga109', 'data/T91','data/historical/LR', 'data/DIV2K/DIV2K_valid_HR'}
 
@@ -180,11 +180,6 @@ def adding_to_the_set4():
         img = dir_list[inside_dataset]
         return img
 
-
-
-
-
-
 ind = randint(1,4)
 if (ind == 1):
     while len(training_set) < num_size_training_set:
@@ -207,14 +202,7 @@ if (ind == 4):
         if img not in training_set:
             training_set.append(img)
 
-
-os.mkdir('data/trainingdata')
+if not os.path.exists('data/trainingdata'): 
+    os.mkdir('data/trainingdata')
 for x in range(0,num_size_training_set - 1):
-    file_path = 'data/trainingdata/' + training_set[x]
-
-
-
-
-
-
-
+    file_path = 'data/trainingdata/' + str(training_set[x])
