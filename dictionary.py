35f77fb68098c2550de4a18d11dc1597cfaca83d
Jashbaugh01 <70974430+Jashbaugh01@users.noreply.github.com>
2020-10-17 14:43:00 -0400

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -40,52 +40,67 @@ for image_name in train_names:
     image_scales['original'] = main_dir + '/' + 'DIV2K_train_HR' + '/' + image_name
     for scale in Kscale:
         s = ''
+        sc =''
         if(scale == 'KX2'):
             s = 'x2'
+            sc = 'LRbicx2'
         elif(scale == 'KX3'):
             s = 'x3'
+            sc = 'LRbicx3'
         else:
             s = 'x4'
+            sc = 'LRbicx4'
             
         corr_name = image_name[:4] + s + image_name[4:]
-        image_scales[scale] = main_dir + '/' + 'DIV2K_train_LR_bicubic' + '/' + corr_name
+        image_scales[sc] = main_dir + '/' + 'DIV2K_train_LR_bicubic' + '/' + corr_name
     for scale in Uscale:
         s = ''
+        sc = '' 
         if(scale == 'UX2'):
             s = 'x2'
+            sc = 'LRunkx2'
         elif(scale == 'UX3'):
             s = 'x3'
+            sc = 'LRunkx2'
         else:
             s = 'x4'
+            sc = 'LRunkx2'
             
         corr_name = image_name[:4] + s + image_name[4:]
-        image_scales[scale] = main_dir + '/' + 'DIV2K_train_LR_unknown' + '/' + corr_name
+        image_scales[sc] = main_dir + '/' + 'DIV2K_train_LR_unknown' + '/' + corr_name
     images[image_name] = image_scales
 for image_name in valid_names:
     image_scales = {}
     image_scales['original'] = main_dir + '/' + 'DIV2K_valid_HR' + '/' + image_name
     for scale in Kscale:
         s = ''
+        sc = ''
         if(scale == 'KX2'):
             s = 'x2'
+            sc = 'LRbicx2'
         elif(scale == 'KX3'):
             s = 'x3'
+            sc = 'LRbicx3'
         else:
             s = 'x4'
+            sc = 'LRbicx4'
             
         corr_name = image_name[:4] + s + image_name[4:]
-        image_scales[scale] = main_dir + '/' + 'DIV2K_valid_LR_bicubic' + '/' + corr_name
+        image_scales[sc] = main_dir + '/' + 'DIV2K_valid_LR_bicubic' + '/' + corr_name
     for scale in Uscale:
         s = ''
+        sc = ''
         if(scale == 'UX2'):
             s = 'x2'
+            sc = 'LRunkx2'
         elif(scale == 'UX3'):
             s = 'x3'
+            sc = 'LRunkx3'
         else:
             s = 'x4'
-            
+            sc = 'LRunkx4'
         corr_name = image_name[:4] + s + image_name[4:]
-        image_scales[scale] = main_dir + '/' + 'DIV2K_valid_LR_unknown' + '/' + corr_name        
+        image_scales[sc] = main_dir + '/' + 'DIV2K_valid_LR_unknown' + '/' + corr_name        
     images[image_name] = image_scales
 
 
