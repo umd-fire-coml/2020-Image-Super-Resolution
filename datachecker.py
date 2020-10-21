2a97c47964d2109cc178bf6b535e673f9baa3aaa
Allen Tu <atu1@umd.edu>
2020-10-21 12:00:18 -0700

    Processor download script (#25)
    * Create datadownloader.ipynb
    
    * Download .zip datasets
    
    datadownloader.ipynb downloads all .zip files from classical_SR_training Google Drive.
    
    * Unzip datasets
    
    Temporarily updated .gitignore to ignore manually downloaded data.
    
    datadownloader.ipynb now downloads and unzips all datasets.
    
    * Download and unzip DIV2K
    
    Script now downloads and unzips DIV2K
    
    * Created datadownloader.py and updated datadownloader.ipynb
    
    Created datadownloader.py and added verified code.
    
    Fixed datadownloader.ipynb unzipping DIV2K files to data instead of data/DIV2K.
    
    * datadownloader.py calls datachecker.py
    
    datadownloader.py now calls datachecker.py after all of the datasets are downloaded and unzipped.
    
    Updated datachecker.py to be callable from both the command line and datadownloader.py.
    
    * Fixed download_url chunking
    
    Added chunk size to download_url in order to prevent  downloads from never writing.
    
    Minor print formatting revisions.
    
    * Changed chunk size
    
    Changed chunk size in download_url to match save_response_content. Differences in speed appear to be negligible, the program uses less memory and has less chance of hanging up.
    
    * Update datasets.txt
    
    Update datasets.txt to include directions, thorough descriptions, and links.
    
    * Update .gitignore
    
    Remove data2/ because it no longer exists.
    
    * Undelete datagenerator.py
    
    Was not included in previous pull (?)
    
    * Undelete datagenerator.py
    
    Was not included in pull before starting (?)
    
    * Undelete datagenerator.py
    
    Was not included in pull (?)
    
    * Resolve changes
    
    * Resolve changes
    
    * Resolve conflicts

--- datachecker.py
+++ datachecker.py
@@ -2,122 +2,125 @@ import sys, glob
 from os import path
 
 # Checks if a directory contains the correct number of .PNGs. 
-def png_checker(path, images_required):
+def png_checker(path, images_required, verbose):
     images = len(glob.glob1(path,'*.png'))
     if images != images_required:
         print(path + ' is INVALID! Contains ' + str(images) + ' images instead of ' + str(images_required) + ' images.')
     elif verbose:
         print(path + ' is complete.')
 # Checks if single directory datasets are complete. 
-def directory_checker(dataset, images):    
+def directory_checker(dataset, images, verbose):    
     dataset_path = 'data/' + dataset
     if verbose == 1: print('Checking ' + dataset + ' dataset...')
     if path.exists(dataset_path):
-        png_checker(dataset_path, images)
+        png_checker(dataset_path, images, verbose)
     else:
         print(dataset + ' is MISSING!')
 
-print('DATA CHECKER')
-# All messages are shown by default. Success messages can be turned off to make failures easier to read. 
-verbose = 1
-if len(sys.argv) == 2:
-    if sys.argv[1] == 'off':
-        verbose = 0
-if verbose == 1: print('Run \'python datachecker.py off\' to only show failure messages.')
-if verbose == 0: print('Run \'python datachecker.py\' to show all messages.\nEmpty output means all datasets are complete.')
+def checker(verbose):
+    # Check if BSDS100 is a complete dataset. 
+    directory_checker('BSDS100', 100, verbose)
 
-# Check if BSDS100 is a complete dataset. 
-directory_checker('BSDS100', 100)
+    # Check if BSDS200 is a complete dataset. 
+    directory_checker('BSDS200', 200, verbose)
 
-# Check if BSDS200 is a complete dataset. 
-directory_checker('BSDS200', 200)
-
-# Check if DIV2K is a complete dataset. 
-if verbose == 1: print('Checking DIV2K dataset...')
-dataset_path = 'data/DIV2K'
-if path.exists(dataset_path):
-    # Check if DIV2K contains all required low-resolution subsets.
-    subsets_LR = ['DIV2K_train_LR_bicubic', 'DIV2K_train_LR_unknown', 'DIV2K_valid_LR_bicubic', 'DIV2K_valid_LR_unknown']
-    for subset in subsets_LR:
-        subset_path = dataset_path + '/' + subset
-        if path.exists(subset_path):
-            # Check if each subset contains all required scales. 
-            scales = ['X2', 'X3', 'X4']
-            for scale in scales:
-                scale_path = subset_path + '/' + scale
-                if path.exists(scale_path):
-                    # Check each scale contains the required number of images. 
-                    if scale_path.find('train') != -1:
-                        png_checker(scale_path, 800)
-                    # Valid datasets contain 100 images. 
+    # Check if DIV2K is a complete dataset. 
+    if verbose == 1: print('Checking DIV2K dataset...')
+    dataset_path = 'data/DIV2K'
+    if path.exists(dataset_path):
+        # Check if DIV2K contains all required low-resolution subsets.
+        subsets_LR = ['DIV2K_train_LR_bicubic', 'DIV2K_train_LR_unknown', 'DIV2K_valid_LR_bicubic', 'DIV2K_valid_LR_unknown']
+        for subset in subsets_LR:
+            subset_path = dataset_path + '/' + subset
+            if path.exists(subset_path):
+                # Check if each subset contains all required scales. 
+                scales = ['X2', 'X3', 'X4']
+                for scale in scales:
+                    scale_path = subset_path + '/' + scale
+                    if path.exists(scale_path):
+                        # Check each scale contains the required number of images. 
+                        if scale_path.find('train') != -1:
+                            png_checker(scale_path, 800, verbose)
+                        # Valid datasets contain 100 images. 
+                        else:
+                            png_checker(scale_path, 100, verbose)
                     else:
-                        png_checker(scale_path, 100)
-                else:
-                    print(scale_path + ' is MISSING!')
-        else:
-            print(subset_path + ' is MISSING!')
-    # Check if DIV2K contains all required high-resolution subsets.
-    subsets_HR = ['DIV2K_train_HR', 'DIV2K_valid_HR']
-    for subset in subsets_HR:
-        subset_path = dataset_path + '/' + subset
-        if path.exists(subset_path):
-            # Check if each subset contains the required number of images. 
-            if subset_path.find('train') != -1:
-                png_checker(subset_path, 800)
-            # Valid datasets contain 100 images. 
+                        print(scale_path + ' is MISSING!')
             else:
-                png_checker(subset_path, 100)
-        else:
-            print(subset_path + ' is MISSING!')
-else:
-    print('DIV2K is MISSING!')
+                print(subset_path + ' is MISSING!')
+        # Check if DIV2K contains all required high-resolution subsets.
+        subsets_HR = ['DIV2K_train_HR', 'DIV2K_valid_HR']
+        for subset in subsets_HR:
+            subset_path = dataset_path + '/' + subset
+            if path.exists(subset_path):
+                # Check if each subset contains the required number of images. 
+                if subset_path.find('train') != -1:
+                    png_checker(subset_path, 800, verbose)
+                # Valid datasets contain 100 images. 
+                else:
+                    png_checker(subset_path, 100, verbose)
+            else:
+                print(subset_path + ' is MISSING!')
+    else:
+        print('DIV2K is MISSING!')
 
-# Check if General100 is a complete dataset. 
-directory_checker('General100', 100)
+    # Check if General100 is a complete dataset. 
+    directory_checker('General100', 100, verbose)
 
-# Check if historical is a complete dataset. 
-if verbose == 1: print('Checking historical dataset...')
-dataset_path = 'data/historical/LR'
-if path.exists(dataset_path):
-    png_checker(dataset_path, 10)
-else:
-    print(historical + ' is MISSING!')
+    # Check if historical is a complete dataset. 
+    if verbose == 1: print('Checking historical dataset...')
+    dataset_path = 'data/historical/LR'
+    if path.exists(dataset_path):
+        png_checker(dataset_path, 10, verbose)
+    else:
+        print(historical + ' is MISSING!')
+
+    # Check if manga109 is a complete dataset. 
+    directory_checker('manga109', 109, verbose)    
+
+    # Check if Set5 is a complete dataset. 
+    if verbose == 1: print('Checking Set5 dataset...')
+    dataset_path = 'data/Set5'
+    if path.exists(dataset_path):
+        # Check if DIV2K contains all required low-resolution subsets.
+        subsets_LR = ['GTmod12', 'LRbicx2', 'LRbicx3', 'LRbicx4', 'original']
+        for subset in subsets_LR:
+            subset_path = dataset_path + '/' + subset
+            if path.exists(subset_path):
+                png_checker(subset_path, 5, verbose)
+            else:
+                print(subset_path + ' is MISSING!')
+    else:
+        print('Set5 is MISSING!')
+
+    # Check if Set14 is a complete dataset.
+    if verbose == 1: print('Checking Set14 dataset...')
+    dataset_path = 'data/Set14'
+    if path.exists(dataset_path):
+        # Check if DIV2K contains all required low-resolution subsets.
+        subsets_LR = ['GTmod12', 'LRbicx2', 'LRbicx3', 'LRbicx4', 'original']
+        for subset in subsets_LR:
+            subset_path = dataset_path + '/' + subset
+            if path.exists(subset_path):
+                png_checker(subset_path, 14, verbose)
+            else:
+                print(subset_path + ' is MISSING!')
+    else:
+        print('Set14 is MISSING!')
+
+    # Check if T91 is a complete dataset. 
+    directory_checker('T91', 91, verbose)    
+
+    # Check if urban100 is a complete dataset. 
+    directory_checker('urban100', 100, verbose)   
     
-# Check if manga109 is a complete dataset. 
-directory_checker('manga109', 109)    
-
-# Check if Set5 is a complete dataset. 
-if verbose == 1: print('Checking Set5 dataset...')
-dataset_path = 'data/Set5'
-if path.exists(dataset_path):
-    # Check if DIV2K contains all required low-resolution subsets.
-    subsets_LR = ['GTmod12', 'LRbicx2', 'LRbicx3', 'LRbicx4', 'original']
-    for subset in subsets_LR:
-        subset_path = dataset_path + '/' + subset
-        if path.exists(subset_path):
-            png_checker(subset_path, 5)
-        else:
-            print(subset_path + ' is MISSING!')
-else:
-    print('Set5 is MISSING!')
-    
-# Check if Set14 is a complete dataset.
-if verbose == 1: print('Checking Set14 dataset...')
-dataset_path = 'data/Set14'
-if path.exists(dataset_path):
-    # Check if DIV2K contains all required low-resolution subsets.
-    subsets_LR = ['GTmod12', 'LRbicx2', 'LRbicx3', 'LRbicx4', 'original']
-    for subset in subsets_LR:
-        subset_path = dataset_path + '/' + subset
-        if path.exists(subset_path):
-            png_checker(subset_path, 14)
-        else:
-            print(subset_path + ' is MISSING!')
-else:
-    print('Set14 is MISSING!')
-
-# Check if T91 is a complete dataset. 
-directory_checker('T91', 91)    
-
-# Check if urban100 is a complete dataset. 
-directory_checker('urban100', 100)   
+if __name__ == "__main__":    
+    print('DATA CHECKER')
+    # All messages are shown by default. Success messages can be turned off to make failures easier to read. 
+    verbose = 1
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'off':
+            verbose = 0
+    if verbose == 1: print('Run \'python datachecker.py off\' to only show failure messages.')
+    if verbose == 0: print('Run \'python datachecker.py\' to show all messages.\nEmpty output means all datasets are complete.')
+    checker(verbose)
---
+++ datadownloader.py
@@ -0,0 +1,127 @@
+import requests, os, zipfile, stat, re, datachecker
+
+data_directory = 'data'
+
+# 'DatasetName' : 'File ID'
+classical_SR_datasets = {
+    'BSDS100': '1EWEsfsgElkNvOcJwZLDe2TeDIMhr6SpH',
+    'BSDS200': '1hIYAIODXT6GksNuk0EHiwgnVxZlDHUAI',
+    'General100': '1Y4R8STXyPcOOykutbAJpMsH5O5n2NdFF',
+    'historical': '17Rq-4gm1_rJX3KB2jolcqMGWiSSmQIWz',
+    'manga109': '15cAVM4BJtSGpduLufqDqfQV75m-Pfepi',
+    'Set5': '1RtyIeUFTyW8u7oa4z7a0lSzT3T1FwZE9',
+    'Set14': '1vsw07sV8wGrRQ8UARe2fO5jjgy9QJy_E',
+    'T91': '1dfsToAYgecVARKjw2wtQS5tsn6pzG6pr',
+    'urban100': '1XaY-tnBP_z21WKgOCeXBa9r-KJyBMbgZ'
+}
+
+# List of DIV2K dataset names
+div2k_datasets = ['DIV2K_train_LR_bicubic_X2', 'DIV2K_train_LR_bicubic_X3', 'DIV2K_train_LR_bicubic_X4',
+                  'DIV2K_train_LR_unknown_X2', 'DIV2K_train_LR_unknown_X3', 'DIV2K_train_LR_unknown_X4',
+                  'DIV2K_valid_LR_bicubic_X2', 'DIV2K_valid_LR_bicubic_X3', 'DIV2K_valid_LR_bicubic_X4',
+                  'DIV2K_valid_LR_unknown_X2', 'DIV2K_valid_LR_unknown_X3', 'DIV2K_valid_LR_unknown_X4',
+                  'DIV2K_train_HR', 'DIV2K_valid_HR']
+
+# Downloads files from Google Drive.
+def download_google(id, destination):
+    URL = 'https://docs.google.com/uc?export=download'
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
+
+# Gets token
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+# Writes chunks
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+# Downloads files from url
+def download_url(url, save_path, chunk_size=128):
+    r = requests.get(url, stream=True)
+    with open(save_path, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+
+print('DATA DOWNLOADER')
+
+# Make data directory
+if not os.path.isdir(data_directory):
+    os.mkdir(data_directory)
+
+# Download and unzip all classical SR datasets
+for dataset in classical_SR_datasets:
+    dataset_directory = data_directory + '/' + dataset
+    if os.path.isdir(dataset_directory):
+        print(dataset_directory + ' already exists.')
+    else:
+        # Download .zip files from Google Drive
+        file_id = classical_SR_datasets[dataset]
+        zip_path = data_directory + '/' + dataset + '.zip'
+        if not os.path.isfile(dataset + '.zip'):
+            print("Downloading " + dataset + '.zip')
+            download_google(file_id, zip_path)
+        # Unzip .zip files
+        print("Unzipping " + dataset + '.zip')
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(data_directory)
+        # Delete .zip file
+        print("Deleting " + dataset + '.zip')
+        os.remove(zip_path)
+    print()
+
+# Make DIV2K directory
+div2k_directory = data_directory + '/' + 'DIV2K'
+if not os.path.isdir(div2k_directory):
+    os.mkdir(div2k_directory)
+
+# Downloads and unzips DIV2K dataset
+for dataset in div2k_datasets:
+    # Check if directory already exists
+    dataset_search = re.search(r'(DIV2K_(train|valid)_LR_(bicubic|unknown))_(X[2,3,4])', dataset)
+    if dataset_search:
+        # LR scale
+        dataset_path = div2k_directory + '/' + dataset_search[1] + '/' + dataset_search[4]
+    else:
+        # HR
+        dataset_path = div2k_directory + '/' + dataset
+    if os.path.isdir(dataset_path):
+        print(dataset_path + ' already exists.')
+    else:
+        # Download .zip files from DIV2K website
+        zip_path = div2k_directory + '/' + dataset + '.zip'
+        if not os.path.isfile(zip_path):
+            url = 'http://data.vision.ee.ethz.ch/cvl/DIV2K/' + dataset + '.zip'
+            print('Downloading ' + dataset + '.zip')
+            download_url(url, zip_path, 32768)
+        # Unzip .zip files
+        print("Unzipping " + dataset + '.zip')
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(div2k_directory)
+        # Delete .zip file
+        print("Deleting " + dataset + '.zip')
+        os.remove(zip_path)
+    print()
+
+# Run datachecker.py with no success messages.
+print("Finished!\n\nRunning datachecker.py. Empty output means no errors.\nErrors:")
+datachecker.checker(verbose = 0)
+print('\nRun datascaler.m after fixing all errors.')
